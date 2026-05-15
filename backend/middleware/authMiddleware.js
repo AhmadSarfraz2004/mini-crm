0b14cf1360dfdcea56e47d121923aeeed8729408
AhmadSarfraz2004 <iahmadsarfraz2004@gmail.com>
2026-05-16 01:03:03 +0500

    Initial commit - backend setup

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+// Middleware to protect routes
+const protect = async (req, res, next) => {
+    try {
+        let token;
+
+        if (
+            req.heade.authorization &&
+            req.headers.authorization.startsWith("Bearer")
+        ) {
+            token = req.headers.authorization.split(" ")[1];
+        }
+
+        if (!token) {
+            return res.status(401).json({ message: "Not authorized, no token" });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = await User.findById(decoded.id).select("-password");
+        next();
+    } catch (error) {
+        res.status(401).json({ message: "Not authorized, token failed" });
+    }
+};
+
+module.exports = protect;
